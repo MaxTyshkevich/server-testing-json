5d038b6628c390100cb4bf5a4d4ff473679304bc
Max Tyshkevich <2842778@gmail.com>
2024-10-29 14:38:33 +0300

    error cors fix

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const fs = require('fs');
 const jsonServer = require('json-server');
 const path = require('path');
 const server = jsonServer.create();
-
+const cors = require('cors')
 const router = jsonServer.router(path.resolve(__dirname, 'db.json'));
 
+
+server.use(cors())
 server.use(jsonServer.defaults({}));
 server.use(jsonServer.bodyParser);
 
